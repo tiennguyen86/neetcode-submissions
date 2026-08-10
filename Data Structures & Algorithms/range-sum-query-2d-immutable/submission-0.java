4f23a07013a64f059f7338e07dcaf957cb5fad50
Nguyễn Hữu Tiến <tien86291@gmail.com>
2026-08-10 19:35:33 +0700

    Add: range-sum-query-2d-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.java
@@ -0,0 +1,18 @@
+public class NumMatrix {
+
+    private int[][] matrix;
+
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int res = 0;
+        for (int r = row1; r <= row2; r++) {
+            for (int c = col1; c <= col2; c++) {
+                res += matrix[r][c];
+            }
+        }
+        return res;
+    }
+}
