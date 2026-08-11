a9fc59ec6f6f445cf0c1ed6d5cf12b568a69dc16
Nguyễn Hữu Tiến <tien86291@gmail.com>
2026-08-11 17:34:32 +0700

    Add: first-missing-positive - submission-6

---
+++ Data Structures & Algorithms/first-missing-positive/submission-6.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Bước 1 + 2:
+        // Đưa số x về đúng index x - 1
+        for (int i = 0; i < n; i++) {
+            while (
+                nums[i] >= 1 &&
+                nums[i] <= n &&
+                nums[nums[i] - 1] != nums[i]
+            ) {
+                int correctIndex = nums[i] - 1;
+
+                // Swap
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            }
+        }
+
+        // Bước 3:
+        // Tìm vị trí đầu tiên không chứa i + 1
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // Nếu 1 -> n đều xuất hiện
+        return n + 1;
+    }
+}
