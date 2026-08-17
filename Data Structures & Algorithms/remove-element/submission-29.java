19aab4fea1a38c2c761bf496fca3119918499960
Nguyễn Hữu Tiến <tien86291@gmail.com>
2026-08-17 16:26:47 +0700

    Add: remove-element - submission-29

---
+++ Data Structures & Algorithms/remove-element/submission-29.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int i = 0;
+
+        while (i < n) {
+          if (nums[i] == val) {
+            nums[i] = nums[--n];
+          }
+          else {
+            i ++;
+          }
+        }
+
+
+        return n;
+
+    }
+}
