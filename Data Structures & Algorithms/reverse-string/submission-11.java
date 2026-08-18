23f82d76fd21bb3b299a530949d0b3a19292bdcd
Nguyễn Hữu Tiến <tien86291@gmail.com>
2026-08-19 06:31:16 +0700

    Add: reverse-string - submission-11

---
+++ Data Structures & Algorithms/reverse-string/submission-11.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public void reverseString(char[] s) {
+        int i = 0; 
+        int n = s.length - 1;
+
+        while (i < n) {
+            char temp = s[i];
+            s[i] = s[n];
+            s[n] = temp;
+            i ++;
+            n --;
+        }
+    }
+}
