7325a77ad7ae526f65596ae269c3cb71b83d1b72
Nguyễn Hữu Tiến <tien86291@gmail.com>
2026-08-25 14:44:10 +0700

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l = 0, r = m * n - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            int midValue = matrix[row][col];
+
+            if (midValue == target) {
+                return true;
+            }
+            else if (midValue < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+}
