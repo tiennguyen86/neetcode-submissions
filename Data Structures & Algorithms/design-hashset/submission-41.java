b4e6ec1199ee93b40457d42ec91cd41dec173563
Nguyễn Hữu Tiến <tien86291@gmail.com>
2026-08-26 17:57:24 +0700

    Add: design-hashset - submission-41

---
+++ Data Structures & Algorithms/design-hashset/submission-41.java
@@ -0,0 +1,58 @@
+class MyHashSet {
+   class ListNode {
+    int key;
+    ListNode next;
+
+    ListNode (int key) {
+        this.key = key;
+    } 
+   }
+
+    private final ListNode[] set;
+
+    public MyHashSet () {
+        set = new ListNode[10000];
+        for (int i = 0; i < set.length; i ++) {
+            set[i] = new ListNode(0);
+        }
+    }
+
+    public void add(int key) {
+        int index = key % set.length;
+        ListNode cur = set[index];
+
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                return;
+            }
+            cur = cur.next;
+        }
+        cur.next = new ListNode(key);
+    }
+
+    public void remove(int key) {
+        int index = key % set.length;
+        ListNode cur = set[index];
+
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next = cur.next.next;
+                return;
+            }
+            cur = cur.next;
+        }
+    }
+
+    public boolean contains(int key) {
+        int index = key % set.length;
+        ListNode cur = set[index];
+
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+}
